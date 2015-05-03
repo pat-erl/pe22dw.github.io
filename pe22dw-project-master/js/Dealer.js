9a03baf45514e12608915fa76b10110b7d4191f0
Patrik Erlandsson <pe22dw@student.lnu.se>
2015-05-03 20:26:33 +0200

    first

---
+++ pe22dw-project-master/js/Dealer.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var Dealer = {
+    
+    giveCard: function() {
+          
+    },
+      
+    takeCard: function() {
+          
+    },
+      
+    collectCards: function() {
+          
+    },
+      
+    takeChip: function() {
+          
+    },
+      
+    giveChip: function() {
+          
+    }
+};
+
